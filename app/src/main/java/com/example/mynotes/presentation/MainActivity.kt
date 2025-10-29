a001a378d74ec0c4804c877d08e66f43c1d71d44
Android225 <a.krishtofor@gmail.com>
2025-10-29 21:36:24 +0300

    realise EditNoteScreen.kt

--- app/src/main/java/com/example/mynotes/presentation/MainActivity.kt
+++ app/src/main/java/com/example/mynotes/presentation/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.mynotes.presentation.screens.creation.CreateNoteScreen
+import com.example.mynotes.presentation.screens.editing.EditNoteScreen
 import com.example.mynotes.presentation.screens.notes.NoteScreen
 
 class MainActivity : ComponentActivity() {
@@ -12,9 +13,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CreateNoteScreen(
+
+            EditNoteScreen(
+                noteId = 5,
                 onFinished = {}
             )
+
+//            CreateNoteScreen(
+//                onFinished = {}
+//            )
 //            NoteScreen(
 //                onNoteClick = {},
 //                onAddNoteClick = {}
