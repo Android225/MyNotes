ccfa0aefe5c7a9288bf7638eb544b16ea0cd32d8
Android225 <a.krishtofor@gmail.com>
2025-10-29 22:07:14 +0300

    add logic for DateFormatter

--- app/src/main/java/com/example/mynotes/presentation/utils/DateFormater.kt
+++ app/src/main/java/com/example/mynotes/presentation/utils/DateFormater.kt
@@ -1,4 +1,34 @@
 package com.example.mynotes.presentation.utils
 
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.concurrent.TimeUnit
+
+
 object DateFormater {
+
+    private val millisInHour = TimeUnit.HOURS.toMillis(1)
+    private val millisInDay = TimeUnit.DAYS.toMillis(1)
+
+    private val formatDate = SimpleDateFormat.getDateInstance(DateFormat.SHORT)
+
+    fun formatCurrentDate(): String{
+        return formatDate.format(System.currentTimeMillis())
+    }
+
+    fun formatDateToString(timestamp: Long): String{
+        val now = System.currentTimeMillis()
+        val diff = now - timestamp
+
+        return when {
+            diff < millisInHour -> "Just Now"
+            diff < millisInDay -> {
+                val hours = TimeUnit.MILLISECONDS.toHours(diff)
+                "$hours ago"
+            }
+            else -> {
+                formatDate.format(timestamp)
+            }
+        }
+    }
 }
