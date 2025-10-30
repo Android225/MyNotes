bfd100ce7d911d30b0a7ac2f525488f32c26cf8e
Android225 <a.krishtofor@gmail.com>
2025-10-30 15:43:32 +0300

    realise logic in Mapper.kt add and realise NotesDao to interact with the database

--- app/src/main/java/com/example/mynotes/data/Mapper.kt
+++ app/src/main/java/com/example/mynotes/data/Mapper.kt
@@ -1,2 +1,15 @@
 package com.example.mynotes.data
 
+import com.example.mynotes.domain.Note
+
+fun Note.toDbModel(): NoteDbModel {
+    return NoteDbModel(id, title, content, updatedAt, isPinned)
+}
+
+fun NoteDbModel.ToEntity(): Note {
+    return Note(id, title, content, updatedAt, isPinned)
+}
+
+fun List<NoteDbModel>.ToEntities(): List<Note>{
+    return this.map { it.ToEntity() }
+}
